702bf0bf1e2bfd2369a43f2bbba57c6847674d65
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2025-12-17 19:30:15 -0500

    Added a few more comments to color and LinkReference objects

--- common/link_reference.go
+++ common/link_reference.go
@@ -1,5 +1,10 @@
 package common
 
+/*
+LinkReference is an object that references links in a URDF model.
+It is primarily used in objects that reference links (for example,
+in JointElement objects).
+*/
 type LinkReference struct {
 	LinkName string `xml:"link,attr"`
 }
