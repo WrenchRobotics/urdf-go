f3841acc47114f18cab5ab954a1e808d292e2388
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2025-12-17 20:47:20 -0500

    Update documentation for pose package

--- common/joint/type/type.go
+++ common/joint/type/type.go
@@ -1,7 +1,12 @@
+// This package is used to define a form of enum with the internal type JointType.
 package joint_type
 
 import "encoding/xml"
 
+/*
+A form of an "enum" that defines the many types of joints
+that are possible aaccording to the URDF specification.
+*/
 type JointType string
 
 const (
@@ -14,6 +19,20 @@ const (
 	UnknownJoint    JointType = "unknown"
 )
 
+/*
+UnmarshalXMLAttr allows for JointType objects to be decoded from
+the "attributes" of a tag.
+
+For example, consider the following tag from a gripper URDF:
+
+	<joint name="finger_joint" type="revolute">
+
+It assigns the "revolute" joint type using the attribute `type`.
+
+This should not normally be called by the user. It is used to
+implement an interface from the xml package and do efficient decoding
+when we use `xml.Unmarshal`.
+*/
 func (jt *JointType) UnmarshalXMLAttr(attr xml.Attr) error {
 	*jt = JointType(attr.Value)
 	return nil
