94f39d177e231510c9b82d254e12152afa6c1dac
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2025-12-17 19:55:33 -0500

    Added better documentation coverage for cylinder, geometry and implementation

--- common/geometry/box.go
+++ common/geometry/box.go
@@ -5,14 +5,34 @@ import (
 	"github.com/WrenchRobotics/urdf-go/common/pose"
 )
 
+/*
+Box is an object that specifies a box with three lengths
+(length, width, height) as is assumed in the URDF specification.
+
+You should be able to parse any box in a URDF, like this one:
+
+	<box size="0.022 0.00635 0.0375"/>
+*/
 type Box struct {
 	Dimensions pose.Vector3 `xml:"size,attr"`
 }
 
+/*
+Type always returns `geometry_type.Box`.
+
+This method is defined in order to implement (in other words,
+satisfy the requirements of) the `GeometryImplementation` interface in this package.
+*/
 func (b *Box) Type() geometry_type.GeometryType {
 	return geometry_type.Box
 }
 
+/*
+Clears all internal variables for Box (only the Dimensions).
+
+This method is defined in order to implement (in other words,
+satisfy the requirements of) the `GeometryImplementation` interface in this package.
+*/
 func (b *Box) Clear() {
 	b.Dimensions.Clear()
 }
--- common/geometry/cylinder.go
+++ common/geometry/cylinder.go
@@ -2,15 +2,37 @@ package geometry
 
 import geometry_type "github.com/WrenchRobotics/urdf-go/common/geometry/type"
 
+/*
+Cylinder is an object which exactly represents a Cylinder object
+according to the URDF specification.
+
+It uses the xml encoding decorators so that you can parse xml snippets without adding more code.
+
+You should be able to parse any cylinder of the form:
+
+	<cylinder radius="0.1" length="1.0" />
+*/
 type Cylinder struct {
 	Radius float64 `xml:"radius,attr"`
 	Length float64 `xml:"length,attr"`
 }
 
+/*
+Always returns `geometry_type.Cylinder`.
+
+This method is defined in order to implement (in other words,
+satisfy the requirements of) the `GeometryImplementation` interface in this package.
+*/
 func (c *Cylinder) Type() geometry_type.GeometryType {
 	return geometry_type.Cylinder
 }
 
+/*
+Clears the internal variables of a Cylinder object (Radius and Length),
+
+This method is defined in order to implement (in other words,
+satisfy the requirements of) the `GeometryImplementation` interface in this package.
+*/
 func (c *Cylinder) Clear() {
 	c.Radius = 0
 	c.Length = 0
